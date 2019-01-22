a9afd5c87bebbf3cee456c53896d9372871da953
Veronica Kang <vkang02@gmail.com>
2019-01-21 22:55:23 -0800

    [Task] Base Webpack Setup

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js']
+    }
+}
